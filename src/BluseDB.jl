34251c988ddb5e2d8a3a273539a7e665d1a7e83f
David MacMahon <davidm@astro.berkeley.edu>
2021-01-10 22:13:44 -0800

    Add loading of database credentials and schema
    Credentials are loaded from `~/.blusedb.yml`.

--- src/BluseDB.jl
+++ src/BluseDB.jl
@@ -1,5 +1,9 @@
 module BluseDB
 
-greet() = print("Hello World!")
+#import Pkg
+import YAML
+
+load_credentials() = YAML.load_file(joinpath(ENV["HOME"], ".blusedb.yml"), dicttype=Dict{Symbol,Any})
+load_schema() = YAML.load_file(joinpath(@__DIR__, "schema.yml"))
 
 end # module
