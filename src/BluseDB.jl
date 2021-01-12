3b930c1954e3cafff77583f431ea7c02b62e111d
David MacMahon <davidm@astro.berkeley.edu>
2021-01-11 18:04:41 -0800

    Flesh out database connectivity, schema creation

--- src/BluseDB.jl
+++ src/BluseDB.jl
@@ -1,9 +1,50 @@
 module BluseDB
 
-#import Pkg
+using MySQL
 import YAML
+import Base: @kwdef
 
-load_credentials() = YAML.load_file(joinpath(ENV["HOME"], ".blusedb.yml"), dicttype=Dict{Symbol,Any})
+@enum UserLevel admin user reader
+
+function load_credentials()
+  credfile = joinpath(ENV["HOME"], ".blusedb.yml")
+  YAML.load_file(credfile, dicttype=Dict{Symbol,Any})
+end
+
+function load_credentials(userlevel::UserLevel)
+  creds = load_credentials()
+  if haskey(creds, Symbol(userlevel))
+    creds = merge(creds, creds[Symbol(userlevel)])
+  end
+  creds
+end
+
 load_schema() = YAML.load_file(joinpath(@__DIR__, "schema.yml"))
+
+function connect(userlevel::UserLevel; kwargs...)::DBInterface.Connection
+  creds = load_credentials(userlevel)
+
+  host = get(creds, :host, "localhost")
+  username = get(creds, :username, "")
+  password = get(creds, :password, "")
+  database = get(creds, :database, "blusedb")
+
+  @info "connecting as $username/$password@$host using database $database"
+  conn = DBInterface.connect(MySQL.Connection,
+                             host, username, password; db=database, kwargs...)
+end
+
+function create_schema()
+  conn = connect(admin)
+  try
+    schema = load_schema()
+    for sql in schema
+      DBInterface.execute(conn, sql)
+    end
+  finally
+    DBInterface.close!(conn)
+  end
+  nothing
+end
 
 end # module
